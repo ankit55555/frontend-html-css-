1b634d5d9cc36f347459115c2be5da4083ef78f7
ankit5555 <islelano55555@gmail.com>
2022-06-25 14:40:23 +0530

    folder added

---
+++ counter app/main.js
@@ -0,0 +1,14 @@
+// let saveEl = document.getElementById("save-el")
+
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+// function increment() {
+//     countEl.innerText = (parseInt(countEl.innerText)+1)        
+// }
+
+// function save() {
+// saveEl.innerText = "-" + countEl.innerText 
+
+// }
+
